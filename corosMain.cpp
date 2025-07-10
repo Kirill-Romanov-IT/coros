67623010ddcc7a0477ab555f0591c5945a1d1395
Your <k.romanov@corp.sg.local>
2025-07-10 17:15:49 +0300

    Update rendering settings and UI layout: change background color to white in corosMain.cpp, adjust Border thickness in DirectXPage.xaml, and set rotation speed to zero in Sample3DSceneRenderer.cpp.

--- corosMain.cpp
+++ corosMain.cpp
@@ -114,7 +114,7 @@ bool corosMain::Render()
 	context->OMSetRenderTargets(1, targets, m_deviceResources->GetDepthStencilView());
 
 	// Clear the back buffer and depth stencil view.
-	context->ClearRenderTargetView(m_deviceResources->GetBackBufferRenderTargetView(), DirectX::Colors::CornflowerBlue);
+	context->ClearRenderTargetView(m_deviceResources->GetBackBufferRenderTargetView(), DirectX::Colors::White);
 	context->ClearDepthStencilView(m_deviceResources->GetDepthStencilView(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 
 	// Render the scene objects.
